cc5874dcb40076b8232e55ea842fddf5376481ca
Vagiz <vahiz.urazov@chisw.com>
2019-07-09 22:01:41 +0300

    fix and refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import TodoList from "./components/Todolist";
-import TodoFooter from "./components/TodoFooter";
+import React, { Component } from 'react';
+import TodoList from './components/Todolist';
+import TodoFooter from './components/TodoFooter';
 
 class App extends Component {
   constructor(...props) {
@@ -8,9 +8,7 @@ class App extends Component {
     this.state = {
       itemsList: [],
       visibleItems: [],
-      view: "all",
-      editing: null,
-      tempValue: "",
+      view: 'all',
       isAllChecked: () => {
         if (
           this.state.itemsList.filter(el => el.checked).length ===
@@ -18,7 +16,7 @@ class App extends Component {
         ) {
           return true;
         } else return false;
-      }
+      },
     };
 
     this.changeFilterState = this._changeFilterState.bind(this);
@@ -27,25 +25,49 @@ class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevState.itemsList !== this.state.itemsList) {
       this.updateViewList();
+      this.saveState();
     }
   }
+  componentDidMount() {
+    this.setState({
+      itemsList: this.restoreState() || [],
+    });
+  }
+
+  saveState = () => {
+    try {
+      return localStorage.setItem(
+        `todo-itemList`,
+        JSON.stringify(this.state.itemsList),
+      );
+    } catch (e) {
+      console.log('ERROR');
+    }
+  };
+  restoreState = () => {
+    try {
+      return JSON.parse(localStorage.getItem(`todo-itemList`));
+    } catch (e) {
+      console.log('ERROR');
+    }
+  };
+//----------------------------------------------header component
   handleSubmit = e => {
     let inputValue = e.target.value.trim();
-    if (e.keyCode !== 13 || inputValue === "") return;
+    if (e.keyCode !== 13 || inputValue === '') return;
     this.setState(prevState => ({
       itemsList: prevState.itemsList.concat({
         label: inputValue,
         checked: false,
-        id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(36)
-      })
+        id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(36),
+      }),
     }));
-    e.target.value = "";
-    this.updateViewList();
+    e.target.value = '';
   };
-
+//----------------------------------------------
   deleteItem = id => {
     this.setState(prevState => ({
-      itemsList: prevState.itemsList.filter(item => item.id !== id)
+      itemsList: prevState.itemsList.filter(item => item.id !== id),
     }));
   };
 
@@ -56,20 +78,19 @@ class App extends Component {
           el.label = text;
         }
         return el;
-      })
+      }),
     }));
   };
 
-  checkItem = id => {
+  checkItem = id => () => {
     this.setState(prevState => ({
       itemsList: prevState.itemsList.map(el => {
         if (el.id === id) {
           el.checked = !el.checked;
         }
         return el;
-      })
+      }),
     }));
-    this.updateViewList();
   };
 
   selectAll = () => {
@@ -78,14 +99,14 @@ class App extends Component {
         itemsList: prevState.itemsList.map(el => {
           el.checked = false;
           return el;
-        })
+        }),
       }));
     } else {
       this.setState(prevState => ({
         itemsList: prevState.itemsList.map(el => {
           el.checked = true;
           return el;
-        })
+        }),
       }));
     }
   };
@@ -93,60 +114,55 @@ class App extends Component {
   clearCompleted = () => {
     this.setState(prevState => ({
       itemsList: prevState.itemsList.filter(el => !el.checked),
-      visibleItems: []
+      visibleItems: [],
     }));
   };
   updateViewList = () => {
     switch (this.state.view) {
-      case "active":
+      case 'active':
         this.setState({
-          visibleItems: this.state.itemsList.filter(el => !el.checked)
+          visibleItems: this.state.itemsList.filter(el => !el.checked),
         });
         break;
-      case "completed":
+      case 'completed':
         this.setState({
-          visibleItems: this.state.itemsList.filter(el => el.checked)
+          visibleItems: this.state.itemsList.filter(el => el.checked),
         });
         break;
       default:
         this.setState({
-          visibleItems: this.state.itemsList
+          visibleItems: this.state.itemsList,
         });
     }
   };
 
   _changeFilterState = e => {
     switch (e.target.hash) {
-      case "#/active":
+      case '#/active':
         this.setState(prevState => ({
-          view: "active",
-          visibleItems: prevState.itemsList.filter(el => !el.checked)
+          view: 'active',
+          visibleItems: prevState.itemsList.filter(el => !el.checked),
         }));
         break;
-      case "#/completed":
+      case '#/completed':
         this.setState(prevState => ({
-          view: "completed",
-          visibleItems: prevState.itemsList.filter(el => el.checked)
+          view: 'completed',
+          visibleItems: prevState.itemsList.filter(el => el.checked),
         }));
         break;
       default:
         this.setState(prevState => ({
-          view: "all",
-          visibleItems: prevState.itemsList
+          view: 'all',
+          visibleItems: prevState.itemsList,
         }));
     }
   };
 
-  handleChange = id => () => {
-    this.setState({ editing: id });
-  };
-  handleChangeVal = e => {
-    this.setState({ tempValue: e.target.value });
-  };
-
   render() {
+    // const {isAllChecked} = this.state
     return (
       <section className="todoapp">
+        {/* header in another component + handlesubmit */}
         <header className="header">
           <h1>Todos</h1>
           <input
@@ -166,9 +182,6 @@ class App extends Component {
           isAllChecked={this.state.isAllChecked}
           view={this.state.view}
           visibleItems={this.state.visibleItems}
-          handleChange={this.handleChange}
-          editing={this.state.editing}
-          handleChangeVal={this.handleChangeVal}
         />
         <TodoFooter
           itemList={this.state.itemsList}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import TodoTextInput from "./TodoTextInput";
-import classnames from "classnames";
+import React, { Component } from 'react';
+import TodoTextInput from './TodoTextInput';
+import classnames from 'classnames';
 
 export default class TodoItem extends Component {
   constructor(...props) {
     super(...props);
     this.state = {
-      editing: false
+      editing: false,
     };
   }
 
@@ -22,7 +22,6 @@ export default class TodoItem extends Component {
     }
     this.setState({ editing: false });
   };
-  
 
   render() {
     const { todo, newTodo } = this.props;
@@ -43,7 +42,7 @@ export default class TodoItem extends Component {
             type="checkbox"
             id={todo.id}
             checked={todo.checked}
-            onChange={() => newTodo.checked(todo.id)}
+            onChange={newTodo.checked(todo.id)}
           />
           <label onDoubleClick={this.handleDoubleClick}>{todo.label}</label>
           <button
@@ -57,7 +56,7 @@ export default class TodoItem extends Component {
       <li
         className={classnames({
           completed: todo.checked,
-          editing: this.state.editing
+          editing: this.state.editing,
         })}
       >
         {element}
@@ -65,39 +64,3 @@ export default class TodoItem extends Component {
     );
   }
 }
-
-// {nowShow.map((item, i) => {
-//     const classNameEditing = props.editing === item.id ? 'editing' : '';
-//     const classNameChecked = item.checked ? 'completed' : '';
-//     return (
-//       <li
-//         key={item.id}
-//         className={`${classNameChecked} ${classNameEditing}`}
-//       >
-//         <div className="view">
-//           <input
-//             className="toggle"
-//             type="checkbox"
-//             id={item.id}
-//             checked={item.checked}
-//             onChange={() => props.checked(item.id)}
-//           />
-//           <label htmlFor={i} onDoubleClick={props.handleChange(item.id)}>
-//             {item.label}
-//           </label>
-//           <button
-//             onClick={() => props.deleted({ i })}
-//             className="destroy"
-//           />
-//         </div>
-//         <input
-//           // ref="editField"
-//           className="edit"
-//           value={item.label}
-//           // onBlur={this.handleSubmit}
-//           onChange={props.handleChangeVal}
-//           // onKeyDown={this.handleKeyDown}
-//         />
-//       </li>
-//     );
-//   })}
